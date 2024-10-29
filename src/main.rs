2b5a11041f85e7363d726235a85f90da57247090
Yuri Albuquerque <yuridenommus@gmail.com>
2024-10-29 05:41:35 -1000

    Insert into database

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{error::Error, sync::Arc, time::Duration};
 
 use chrono::Utc;
 use chrono_english::{parse_date_string, Dialect};
+use sqlx::{sqlite::SqlitePoolOptions, Pool, Sqlite};
 use teloxide::{
     macros::BotCommands, prelude::*, types::ReplyParameters,
     utils::command::BotCommands as BCommands,
@@ -20,7 +21,7 @@ enum Command {
     RemindMe(String),
 }
 
-async fn answer(bot: Arc<Bot>, msg: Message, cmd: Command) -> ResponseResult<()> {
+async fn answer(bot: Arc<Bot>, msg: Message, cmd: Command, db: Pool<Sqlite>) -> ResponseResult<()> {
     match cmd {
         Command::Help => {
             bot.send_message(msg.chat.id, Command::descriptions().to_string())
@@ -31,6 +32,20 @@ async fn answer(bot: Arc<Bot>, msg: Message, cmd: Command) -> ResponseResult<()>
                 bot.send_message(msg.chat.id, format!("I'll remind you at {}", date))
                     .reply_parameters(ReplyParameters::new(msg.id))
                     .await?;
+                let Ok(_) = sqlx::query!(
+                    "INSERT INTO messages (reply_to_id, chat_id, when_send) \
+                                              VALUES ($1, $2, $3)",
+                    msg.id.0,
+                    msg.chat.id.0,
+                    date
+                )
+                .execute(&db)
+                .await
+                else {
+                    bot.send_message(msg.chat.id, "Error saving to the database")
+                        .await?;
+                    return Ok(());
+                };
             }
             Err(_) => {
                 bot.send_message(msg.chat.id, "Non recognized date format")
@@ -49,6 +64,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let bot = Arc::new(Bot::from_env());
 
+    let pool = SqlitePoolOptions::new()
+        .max_connections(5)
+        .connect("sqlite:./database.sqlite")
+        .await?;
+
     let f1 = tokio::spawn(async move {
         loop {
             tokio::select! {
@@ -60,7 +80,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     });
 
-    let f2 = Command::repl(bot.clone(), answer);
+    let f2 = Command::repl(bot.clone(), move |bot, msg, cmd| {
+        answer(bot, msg, cmd, pool.clone())
+    });
 
     let _ = join!(f1, f2);
 
